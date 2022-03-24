5fcca99892d3c5f58601ecb3a636c7a5476581f9
Jason Yun <jasonyun@Jasons-MacBook-76.local>
2022-03-24 17:45:07 +0900

    set domain

--- src/main.rs
+++ src/main.rs
@@ -4,61 +4,61 @@ use yew_router::prelude::*;
 // ===================================================================================
 // for {username}.github.io/{repo_name}
 
-#[derive(Clone, Routable, PartialEq)]
-enum RootRoute {
-    #[at("/yew-template-for-github-io/")]
-    Home,
-    #[at("/yew-template-for-github-io/:s")]
-    Route,
-}
+// #[derive(Clone, Routable, PartialEq)]
+// enum RootRoute {
+//     #[at("/yew-template-for-github-io/")]
+//     Home,
+//     #[at("/yew-template-for-github-io/:s")]
+//     Route,
+// }
+
+// #[derive(Clone, Routable, PartialEq)]
+// enum Route {
+//     #[at("/yew-template-for-github-io/about")]
+//     About,
+//     #[not_found]
+//     #[at("/yew-template-for-github-io/404")]
+//     NotFound,
+// }
+
+// fn root_route(routes: &RootRoute) -> Html {
+//     match routes {
+//         RootRoute::Home => html! { <p class="text-4xl">{ "Yew Template" }</p> },
+//         RootRoute::Route => html! {
+//             <Switch<Route> render={Switch::render(switch)} />
+//         },
+//     }
+// }
+
+// fn switch(routes: &Route) -> Html {
+//     match routes {
+//         Route::About => html! { <p>{ "About" }</p> },
+//         Route::NotFound => html! { <p>{ "Not Found" }</p> },
+//     }
+// }
+
+// ===================================================================================
+// for {username}.github.io
 
 #[derive(Clone, Routable, PartialEq)]
 enum Route {
-    #[at("/yew-template-for-github-io/about")]
+    #[at("/")]
+    Home,
+    #[at("/about")]
     About,
     #[not_found]
-    #[at("/yew-template-for-github-io/404")]
+    #[at("/404")]
     NotFound,
 }
 
-fn root_route(routes: &RootRoute) -> Html {
+fn root_route(routes: &Route) -> Html {
     match routes {
-        RootRoute::Home => html! { <p class="text-4xl">{ "Yew Template" }</p> },
-        RootRoute::Route => html! {
-            <Switch<Route> render={Switch::render(switch)} />
-        },
-    }
-}
-
-fn switch(routes: &Route) -> Html {
-    match routes {
+        Route::Home => html! { <p class="text-4xl">{ "Yew Template" }</p> },
         Route::About => html! { <p>{ "About" }</p> },
         Route::NotFound => html! { <p>{ "Not Found" }</p> },
     }
 }
 
-// ===================================================================================
-// for {username}.github.io
-//
-// #[derive(Clone, Routable, PartialEq)]
-//  enum Route {
-//      #[at("/")]
-//      Home,
-//      #[at("/about")]
-//      About,
-//      #[not_found]
-//      #[at("/404")]
-//      NotFound,
-//  }
-
-//  fn root_route(routes: &Route) -> Html {
-//      match routes {
-//          Route::Home => html! { <p class="text-4xl">{ "Yew Template" }</p> },
-//          Route::About => html! { <p>{ "About" }</p> },
-//          Route::NotFound => html! { <p>{ "Not Found" }</p> },
-//      }
-//  }
-//
 // ===================================================================================
 
 /// main root
@@ -72,7 +72,7 @@ fn app() -> Html {
         // </BrowserRouter>
         // ********************************************************
         <BrowserRouter>
-            <Switch<RootRoute> render={Switch::render(root_route)} />
+            <Switch<Route> render={Switch::render(root_route)} />
         </BrowserRouter>
     }
 }
